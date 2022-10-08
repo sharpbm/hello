8f319471efc86e14bdb18fac587154314f09cfd1
Pankaj Sharma <pankaj.sharma1@natwest.com>
2022-10-08 08:58:38 +0000

    test

--- tests/test_nlib.py
+++ tests/test_nlib.py
@@ -15,6 +15,6 @@ def test_group_by_operations():
     data, groupby_column_name, apply_column_name
     """
     expected = {"john":22,"kristen":17,"piers":10,"sam":15}
-    actual = csvops.aggregate_column_name(data="../ext/input.csv", 
+    actual = csvops.aggregate_column_name(data="ext/input.csv", 
             groupby_column_name="first_name", apply_column_name="count")
     assert expected == actual.to_dict()
